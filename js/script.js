5d794bbb7ead4918104bdba6d1dfa7fd3c27fd42
Thomas Bell <thomas.bell@rogers.com>
2018-04-15 21:18:20 -0400

    mobile updates

--- js/script.js
+++ js/script.js
@@ -3,24 +3,27 @@ $(function(){
 });
 
 $(window).scroll(function(){
-  if($(document).scrollTop() > 0)
-{
-    if($('#headercontent').data('size') == 'big')
+  if (/Android|BlackBerry|iPhone|iPad|iPod|webOS/i.test(navigator.userAgent) === false) {
+
+  if($(document).scrollTop() > 0 )
     {
-        $('#headercontent').data('size','small');
-        $('#headercontent').stop().animate({
-            height:'40px'
-        },500);
+      if($('#headercontent').data('size') == 'big')
+        {
+          $('#headercontent').data('size','small');
+          $('#headercontent').stop().animate({
+              height:'40px'
+          },500);
+        }
+    }
+  else
+    {
+      if($('#headercontent').data('size') == 'small')
+        {
+          $('#headercontent').data('size','big');
+          $('#headercontent').stop().animate({
+              height:'100px'
+          },500);
+        }
     }
 }
-else
-  {
-    if($('#headercontent').data('size') == 'small')
-      {
-        $('#headercontent').data('size','big');
-        $('#headercontent').stop().animate({
-            height:'100px'
-        },500);
-      }
-  }
 });
